0f5d6990293a4921dc1c820f9463a099fe49ea4e
Leandro Souza <161175460+lsouzadasilva@users.noreply.github.com>
2025-03-11 09:46:00 -0300

    Atualizar o app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ st.sidebar.divider()
 with st.sidebar:
     paginas = option_menu(
     menu_title = "Menu",
-    options = ["Sobre", "Projetos em Power BI", "Projetos em Streamlit & Plotly", "Projetos OpenAI", "Projetos huggingface", "Certificados & Skills"],
+    options = ["Sobre min", "Projetos em Power BI", "Projetos em Streamlit & Plotly", "Projetos OpenAI", "Projetos huggingface", "Certificados & Skills"],
     icons = ["envelope-at-fill", "bar-chart-fill", "graph-up-arrow", "robot", "emoji-wink-fill", "award"],
     menu_icon ="cast",
     default_index = 0
@@ -44,7 +44,7 @@ st.sidebar.divider()
 st.sidebar.markdown("Desenvolvido por [Leandro Souza](https://br.linkedin.com/in/leandro-souza-313136190)")
 
 
-if paginas == "Sobre":
+if paginas == "Sobre min":
     pagina_inicial()
 elif paginas == "Projetos em Power BI":
     bi()
